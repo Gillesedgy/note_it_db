72b2b8e7bc22ce76a21455be4e13eb45331c9feb
Gillesedgy <edgygilles@pursuit.org>
2023-02-07 12:41:46 -0500

    initial set up

---
+++ app.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+
+// app.use("/notes");
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to ThaNotes");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json("Page not found");
+});
+
+// EXPORT
+module.exports = app;
